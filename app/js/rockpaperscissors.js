6ef9f4d3f800a544674469ab47d41ca8d963036e
Chimerus <jlc370@gmail.com>
2015-11-06 23:21:53 -0500

    unit4 hw

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,14 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return move || getInput();
 }
 
 function getWinner(playerMove,computerMove) {
@@ -40,7 +40,35 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    switch (playerMove) {
+        case "rock":
+        if (computerMove=="rock"){
+            winner = "tie";
+        } else if(computerMove=="paper"){
+            winner = "computer";
+        } else{
+            winner = "player";
+        }
+        break;
+      case "scissors":
+        if (computerMove=="rock"){
+            winner = "computer";
+        } else if(computerMove=="paper"){
+            winner = "player";
+        } else{
+            winner = "tie";
+        }
+        break;
+      case "paper":
+        if (computerMove=="rock"){
+            winner = "player";
+        } else if(computerMove=="paper"){
+            winner = "tie";
+        } else{
+            winner = "computer";
+        }
+        break;
+    }
     return winner;
 }
 
